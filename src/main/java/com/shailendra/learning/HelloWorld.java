02483ab54572e2e0626c75b0b084ab456b383ad0
shailendra210 <thakurshailendrasingh18@gmail.com>
2021-05-24 11:42:18 +0530

    Update HelloWorld.java
    updating commits

--- src/main/java/com/shailendra/learning/HelloWorld.java
+++ src/main/java/com/shailendra/learning/HelloWorld.java
@@ -3,5 +3,6 @@ package com.shailendra.learning;
 public class HelloWorld {
 	public static void main(String args[]) {
 		System.out.println("Hello world");
+		System.out.println("this is change commit");
 	}
 }
