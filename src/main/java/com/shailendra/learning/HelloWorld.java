08dbb4b4cd14388221abd92f2b3a177ea6a94984
thaku <s.b.singh.thakur@gmail.com>
2021-05-24 11:57:20 +0530

    updating changes

--- src/main/java/com/shailendra/learning/HelloWorld.java
+++ src/main/java/com/shailendra/learning/HelloWorld.java
@@ -4,11 +4,7 @@ public class HelloWorld {
 	public static void main(String args[]) {
 		System.out.println("Hello world");
 		System.out.println("this is new commit");
-
-
 		System.out.println("this is change commit");
 		System.out.println("Shailendra show");
-
-
 	}
 }
